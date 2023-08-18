44c258110286bbc321ef5fe2252f06b025f3a9a0
João Engel <106564709+joao-engel@users.noreply.github.com>
2023-08-17 22:53:29 -0300

    init

---
+++ PrimeiroProjeto/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace PrimeiroProjeto {
+internal class Program {
+        static void Main(string[] args) {
+            Retangulo ret = new Retangulo();
+            Console.WriteLine("Informe o tamanho dos lados do quadrado");
+            string[] vet = Console.ReadLine().Split(' ');
+
+            ret.LadoEsq = int.Parse(vet[0]);
+            ret.LadoSup = int.Parse(vet[1]);
+            ret.LadoInf = int.Parse(vet[2]);
+            ret.LadoDir = int.Parse(vet[3]);
+
+            if (ret.Validador()) {
+                if (ret.Comparador()) {
+                    Console.WriteLine("Retangulo!");
+                }
+                else {
+                    Console.WriteLine("Quadrado!");
+                }
+            }
+            else {
+                Console.WriteLine("Não é quadrado e nem retangulo!!");
+            }
+        }
+    }
+}
---
+++ PrimeiroProjeto/Retangulo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeiroProjeto {
+    class Retangulo {
+        public int LadoEsq;
+        public int LadoSup;
+        public int LadoDir;
+        public int LadoInf;
+
+        public bool Validador() {
+            if (LadoEsq == LadoDir && LadoSup == LadoInf) {
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+
+        public bool Comparador() {
+            if (LadoSup != LadoEsq) {
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+    }
+}
